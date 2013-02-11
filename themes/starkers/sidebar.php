6f66286366c414e66c99daaeeebff18bcf1330e8
Chris Macholz <chrismacholz@gmail.com>
2013-02-11 15:02:00 -0500

    Fixed recent posts query in sidebar

--- themes/starkers/sidebar.php
+++ themes/starkers/sidebar.php
@@ -18,7 +18,7 @@
 			$args = array( 'numberposts' => '3' );
 				$recent_posts = wp_get_recent_posts( $args );
 				foreach( $recent_posts as $recent ){
-					if($recent->post_status  == "publish"){ 
+					if( get_post_status ($ID) == 'publish'){ 
 						echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
 					}
 				}
