4a197a93eee8e6776d78dc1a61d5de1916d5cb7e
Tim Macholz <tim@solvingtheweb.com>
2013-05-02 13:28:44 -0400

    analytics update, switched back to classic analytics

--- themes/starkers/parts/shared/html-header.php
+++ themes/starkers/parts/shared/html-header.php
@@ -13,14 +13,17 @@
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
 		<link rel="stylesheet" href="<?php bloginfo( 'template_url' ); ?>/style.css"/>
 		<link type="text/css" rel="stylesheet" href="http://fast.fonts.com/cssapi/6f9f210d-0fc3-44d0-bece-aa8720f67c35.css"/>
-		<script>
-		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		<script type="text/javascript">
 
-		  ga('create', 'UA-40597402-1', 'stanbee.com');
-		  ga('send', 'pageview');
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-40603585-1']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
 
 		</script>
 		<?php wp_head(); ?>
