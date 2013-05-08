013e20dde88be899fc6ca65ee4eb3e374ae0aeb8
Chris Macholz <chris@solvingtheweb.com>
2013-05-07 22:19:02 -0400

    Added pagination to news listing

--- themes/starkers/index.php
+++ themes/starkers/index.php
@@ -16,13 +16,20 @@
 <li>
 		<article>
 			<h1><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-			<time datetime="<?php the_time( 'Y-m-D' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
+			<time datetime="<?php the_time( 'Y-m-D' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time>
 			<?php the_post_video(); ?>
 			<?php the_excerpt(); ?>
 		</article>
 </li>
 <?php endwhile; ?>
 </ul>
+
+	<?php if ( get_previous_posts_link() ) : ?>
+		<div class="nav-next"><?php previous_posts_link( __( '<span class="meta-nav">&laquo;</span> Previous Page') ); ?></div>
+	<?php endif; ?>
+	<?php if ( get_next_posts_link() ) : ?>
+		<div class="nav-previous"><?php next_posts_link( __( 'Next Page <span class="meta-nav">&raquo;</span>') ); ?></div>
+	<?php endif; ?>
 <?php else: ?>
 <h2>No posts to display</h2>
 <?php endif; ?>
